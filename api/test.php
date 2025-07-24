cdf6d05a69ece5cac5935ff35342fa0056079ec7
RicoDaCrook <cem.tek35@outlook.de>
2025-07-24 20:48:30 +0200

    Fix PHP header

--- api/test.php
+++ api/test.php
@@ -1,2 +1,3 @@
 ﻿<?php
-echo json_encode(["status" => "test", "message" => "PHP läuft"]);
+header("Content-Type: application/json");
+echo json_encode(["status" => "ok", "message" => "PHP läuft auf Vercel!"]);
