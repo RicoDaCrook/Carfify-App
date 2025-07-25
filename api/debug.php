c602fdca99651f01960826b1196593088f508a6b
RicoDaCrook <cem.tek35@outlook.de>
2025-07-25 13:45:39 +0200

    Add debug API

---
+++ api/debug.php
@@ -0,0 +1,12 @@
+﻿<?php
+// Zeige ALLE Fehler an
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+header("Content-Type: application/json");
+echo json_encode([
+    "status" => "debug", 
+    "message" => "API Check", 
+    "server" => $_SERVER['REQUEST_URI'],
+    "method" => $_SERVER['REQUEST_METHOD']
+]);
